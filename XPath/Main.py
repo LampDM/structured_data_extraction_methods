45c0736c029a53927ed30859376256ff5f18605c
Lamp <dmvoicestudios@gmail.com>
2019-04-20 10:54:54 +0200

    added json dump

--- XPath/Main.py
+++ XPath/Main.py
@@ -1,11 +1,13 @@
-from lxml import html, etree
+from lxml import html
+import json
+import sys
 
 doc = html.fromstring(open('../WebPages/overstock.com/jewelry01.html').read())
 count = doc.xpath("/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/*")
 
 for i in range(1, len(count) + 1):
     try:
-        titlepath = doc.xpath(
+        title = doc.xpath(
             "/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[{}]/td[2]/a/b".format(
                 i))[0].text
         content = doc.xpath(
@@ -21,7 +23,17 @@ for i in range(1, len(count) + 1):
             "/html/body/table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[{}]/td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span".format(
                 i))[0]
         saving, savprcnt = savingppr.text.split(" ")
-        print([titlepath,content,listprice,price,saving,savprcnt])
+
+        out_json = {
+            'title' : title,
+            'content' : content,
+            'listprice' : listprice,
+            'price' : price,
+            'saving' : saving,
+            'savprcnt' : savprcnt
+        }
+
+        print(json.dumps(out_json, ensure_ascii=False, indent=4), file=sys.stdout)
 
     except Exception as ex:
         pass
