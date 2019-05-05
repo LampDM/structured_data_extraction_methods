c8e59261da9d3220ce598914322ec0afb4b6a7ed
Toni Kocjan <toni.kocjan@poviolabs.com>
2019-05-05 13:55:15 +0200

    CLI

--- RoadRunner/main.swift
+++ RoadRunner/main.swift
@@ -8,8 +8,15 @@
 
 import Foundation
 
+func printInterface() {
+  print("Usage: ./main base_file=path/to/base/file.html reference_file=path/to/reference/file.html [OPTIONS]")
+  print("OPTIONS:")
+  print("  -tag_id=id  Search for a tag with a given `id`. If not found, calculate wrapper on the whole tree")
+  print("  -tree=path/to/file.html  By setting this flag, you don't have to provide `base_file` and `reference_file` arguments. This will inturn dump tree structure of provided file")
+}
+
 func logError(_ message: String) {
-  print("⚠️ " + message)
+  print("⚠️  " + message)
 }
 
 func parseFile(_ file: String) throws -> Tree {
@@ -23,23 +30,52 @@ func parseFile(_ file: String) throws -> Tree {
 let argumentParser = ArgumentParser()
 argumentParser.parseArguments(CommandLine.arguments)
 
+print(CommandLine.arguments)
+if let dumpTreeFile = argumentParser.string(for: "tree") {
+  let tree = try parseFile(dumpTreeFile)
+  print(tree)
+  exit(0)
+}
+
 guard let baseFile = argumentParser.string(for: "base_file") else {
   logError("Missing `base_file` argument!")
+  print()
+  printInterface()
   exit(100)
 }
 
 guard let referenceFile = argumentParser.string(for: "reference_file") else {
   logError("Missing `reference_file` argument!")
+  print()
+  printInterface()
   exit(101)
 }
 
+let targetTag = argumentParser.string(for: "tag_id")
+
 do {
+  print("Parsing base file ...")
   let baseTree = try parseFile(baseFile)
-  let referenceTree = try parseFile(referenceFile)
+  print("Successfuly parsed base file ...")
   
-  let algorithm = RoadRunnerLikeAlgorithm(baseTree: baseTree.elementById("neki")!,
-                                          referenceTree: referenceTree.elementById("neki")!)
+  print("Parsing reference file ...")
+  let referenceTree = try parseFile(referenceFile)
+  print("Successfuly parsed reference file ...")
+  
+  var baseSubTree = baseTree
+  var referenceSubTree = referenceTree
+  if let targetTag = targetTag,
+    let fromBase = baseSubTree.elementById(targetTag),
+    let fromReference = referenceSubTree.elementById(targetTag) {
+    baseSubTree = fromBase
+    referenceSubTree = fromReference
+  }
+  
+  print("Initiating road runner ...")
+  let algorithm = RoadRunnerLikeAlgorithm(baseTree: baseSubTree,
+                                          referenceTree: referenceSubTree)
   let wrapper = algorithm.buildWrapper()
+  print("Wrapper:")
   print(wrapper)
 } catch {
   logError("Failed with error: " + error.localizedDescription)
